9da79eca809d1e78500d501c7802b974c2f1f87a
SivasuriyanM <sivasuriyan.m@ckcet.ac.in>
2025-05-24 19:03:30 +0530

    leetcode 2942

--- leetcode/Tree/BinaryTrees.java
+++ leetcode/Tree/BinaryTrees.java
@@ -80,6 +80,7 @@ public class BinaryTrees {
             this.val = val;
         }
     }
+    
 
     //Binary Tree Level Order traversal
     public List<List<Integer>> levelOrder() {
--- leetcode/arrays/Solution.java
+++ leetcode/arrays/Solution.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
 //26. Remove Duplicates from Sorted Array
 class Solution {
+     public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> resultIndices = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                resultIndices.add(i);
+            }
+        }
+        return resultIndices;
+        
+    }
+    
     public static int removeDuplicates(int[] nums) {
         int in = 0;
         int[] arr=new int[nums.length];
@@ -34,5 +45,8 @@ class Solution {
         int []arr = {0,0,1,1,1,2,2,3,3,4};
         removeDuplicates(arr);
         System.err.println(Arrays.toString(arr));
+        String[] words = {"abc","bcd","aaaa","cbc"};
+        char x = 'a';
+        System.out.println(Arrays.toString(findWordsContaining(words, x)))
     }
 }
